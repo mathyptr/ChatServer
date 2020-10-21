debe9191279e9fc3120ca9a76ee333fea80dad36
Mathyptr <mathyptr@gmail.com>
2020-10-21 18:44:06 +0200

    aggiornamento documentazione

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Chat"},{"p":"<Unnamed>","l":"Client"},{"p":"<Unnamed>","l":"Server"},{"p":"<Unnamed>","l":"ThreadClient"},{"p":"<Unnamed>","l":"ThreadServer"}]
